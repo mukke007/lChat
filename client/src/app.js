8c720a169f43e2c12667f78a32e37ebeddbcb768
lkopi246 <asharma.bhwn@gmail.com>
2020-03-31 08:41:14 -0700

    client boiler plate

---
+++ client/src/app.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Comp from "./components/Comp";
+
+ReactDOM.render(<Comp />, document.getElementById("root"));
---
+++ client/src/components/Comp.js
@@ -0,0 +1,4 @@
+import React from "react";
+const Comp = () => <h1>nice name</h1>;
+
+export default Comp;
